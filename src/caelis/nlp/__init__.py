6aff7ad092a755f7fc4af72d250f881fc7f4120d
BS <bsbalaji101005@gmail.com>
2026-07-29 08:54:13 +0530

    Create __init__.py

---
+++ src/caelis/nlp/__init__.py
@@ -0,0 +1,10 @@
+from caelis.nlp.matcher import IntentMatcher
+from caelis.nlp.intent import IntentResult
+from caelis.nlp.normalizer import normalize_text
+
+
+__all__ = [
+    "IntentMatcher",
+    "IntentResult",
+    "normalize_text",
+]
